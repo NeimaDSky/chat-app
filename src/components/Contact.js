814eb97ebea8c430ab8ab628a8bf2a78eda8b195
Damien Vayssiaire <damien.vayssiaire@gmail.com>
2019-11-15 10:44:29 +0100

    state ok

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import './Chat.css';
 
-function Contact(props) {
-    return (
-    <div className ="Contact">
-        <img src ={props.avatar} className = 'avatar' alt='avatar'/>
-        <div className='status'>
-            <h3 className= 'name'>{props.name}</h3>
-            <p className = 'birthday'>{props.birthday}</p>
-            <p className = 'adress'>{props.adress}</p>
-            <div className="status-bloc">
-                <p className={props.online ? "status-online" : "status-offline"}> </p>
-                <p className="status-text">{props.online ? "Online" : "Offline"}</p>
-               
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            online: this.props.online
+        };
+    }
+
+
+    render() {
+        return (
+            <div className="Contact">
+                <img src={this.props.avatar} className='avatar' alt='avatar' />
+                <div className='status'>
+                    <h3 className='name'>{this.props.name}</h3>
+                    <p className='birthday'>{this.props.birthday}</p>
+                    <p className='adress'>{this.props.adress}</p>
+                    <div className="status-bloc" onClick = {event => {
+                        const newStatus = !this.state.online;
+                        this.setState({ online: newStatus }); 
+                    }} 
+                    >
+                        <span className={this.state.online ? "status-online" : "status-offline"}> </span>
+                        <p className="status-text">{this.state.online ? "online" : "offline"} </p>
+                    
+                        
+
 
             </div>
 
-        </div>
-    </div>
-    );
+                </div >
+            </div >
+        );
+    }
 }
-
 export default Contact;
